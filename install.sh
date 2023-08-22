b4f908445a92e432013b4703c0330f14c1fb15b6
britestak <142447609+britestak@users.noreply.github.com>
2023-08-22 21:50:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,9 @@ git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
 pushd  "$NVM_DIR"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 popd
-chmod +x "$NVM_DIR/nvm.sh"
-. "$NVM_DIR/nvm.sh" --no-use
 fi
-
+chmod +x "$NVM_DIR/nvm.sh"
+"$NVM_DIR/nvm.sh" --no-use
 nvm install "$1"
 echo "SETUP_NODE_NVM_NVM: $NVM_DIR/nvm.sh"
 echo "SETUP_NODE_NVM_NODE: $(which node)"
