a793f6da2899f5c98f6e5d446dbf18bf689c19b5
britestak <142447609+britestak@users.noreply.github.com>
2023-08-22 21:21:39 +0530

    Check if already installed

--- install.sh
+++ install.sh
@@ -2,12 +2,16 @@
 set -e
 export NVM_DIR="$HOME/mynvm"
 export NVM_NODEJS_ORG_MIRROR="$2"
+
+if [ ! -d "$DIRECTORY" ]; then
 git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
 pushd  "$NVM_DIR"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 popd
 chmod +x "$NVM_DIR/nvm.sh"
 . "$NVM_DIR/nvm.sh" --no-use
+fi
+
 nvm install "$1"
 echo "SETUP_NODE_NVM_NVM: $NVM_DIR/nvm.sh"
 echo "SETUP_NODE_NVM_NODE: $(which node)"
