a13679fcbe84f5da3097987b895ee7ac45e85975
britestak <142447609+britestak@users.noreply.github.com>
2023-08-23 19:37:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 set -e
 export NVM_DIR="$HOME/mynvm"
 export NVM_NODEJS_ORG_MIRROR="$2"
+if [ ! -d "$HOME/mynvm" ]; then
 git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
 pushd  "$NVM_DIR"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 popd
+fi
 chmod +x "$NVM_DIR/nvm.sh"
 . "$NVM_DIR/nvm.sh" --no-use
 nvm install "$1"
